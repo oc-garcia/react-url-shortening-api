9e5140ce583d572d666de2c0747b166681b91c3c
oc-garcia <octaviogarcia1988@gmail.com>
2023-02-15 17:29:41 -0300

    Api result is being rendered on screen.

---
+++ src/Components/ShortlyBar/ReturnedLinks/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ReturnedLinks({link1, link2}) {
+  return (
+    <div>
+      <p>{link1}</p>
+      <p>{link2}</p>
+    </div>
+  )
+}
--- src/Components/ShortlyBar/index.jsx
+++ src/Components/ShortlyBar/index.jsx
@@ -1,27 +1,40 @@
 import style from './Shortlybar.module.css'
 import Button from "../Button";
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import ReturnedLinks from './ReturnedLinks';
 
 const ShortlyBar = () => {
+
+const [listOfLinks, setListOfLinks] = useState([])
+
 const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = async input => {
+  console.log(input);
   console.log(errors);
+  let apiEndPoint = "https://api.shrtco.de/v2/shorten?url="
+  let url = `${apiEndPoint}${input.link}`
+  let response = await fetch(url)
+  let data = await response.json();
+  console.log(data)
+setListOfLinks([{link1: data.result.share_link, link2: data.result.short_link2}])
+  }
     return (
         <>
         <div  className={style.background}>
             <form className={style.inputContainer} onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <input 
-                    className={errors.Link ? style.input_error : style.input_ok}
+                     className={errors.link ? style.input_error : style.input_ok}
                      placeholder='Shorten a link here...'
-                     {...register("Link", {required: "Please add a link", pattern: { value: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/i, message: "Looks like thiks is not a valid url"}})}
+                     {...register("link", {required: "Please add a link"})}
                     />
-                    <span>{errors.Link?.message}</span>
+                    <span>{errors.link?.message}</span>
                 </div>
                 <Button text="Shorten it!"/>          
             </form>
         </div>
+        {listOfLinks.map(link => <ReturnedLinks link1={link.link1} link2={link.link2} />)}
         </>
     )
 }
